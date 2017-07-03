72da2c343179c1f3724ed90c21bba5aa910240d0
Siddhartha Chandra <schandra@collectivei.com>
2017-07-02 23:00:54 -0400

    fix: add comparisoon with ancestral node for left sub-tree

--- 9.py
+++ 9.py
@@ -1,3 +1,5 @@
+# Write a function to check that a binary tree is a valid binary search tree
+
 class BinaryTreeNode:
 
     def __init__(self, value):
@@ -13,21 +15,34 @@ class BinaryTreeNode:
         self.right = BinaryTreeNode(value)
         return self.right
 
-def check_bst(node):
-    if node.left:
+def check_bst(node, parent_value = None):
+    if not node.left:
+        res_l = True
+    else:
         if node.left.value <= node.value:
-            check_bst(node.left)
+            res_l = check_bst(node.left, node.value)
         else:
-            return False
-    if node.right:
+            res_l = False
+
+    if not node.right:
+        res_r = True
+    else:
         if node.right.value > node.value:
-            check_bst(node.right)
+            print "%s, %s" % (node.right.value, parent_value)
+            if parent_value == None or node.right.value <= parent_value:
+                res_r = check_bst(node.right, None)
+            else:
+                res_r = False
         else:
-            return False
-    return True
+            res_r = False
+    return res_l and res_r
 
 tree = BinaryTreeNode(50)
-tree.insert_left(25)
-tree.insert_right(51)
+l = tree.insert_left(30)
+l.insert_left(20)
+l.insert_right(60)
+r = tree.insert_right(80)
+r.insert_left(70)
+r.insert_right(90)
 
 print "Is BST: %s" % check_bst(tree)
