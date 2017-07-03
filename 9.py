e886a4405e2d0a6a275844aed895134966c19d5d
Siddhartha Chandra <schandra@collectivei.com>
2017-07-03 17:26:16 -0400

    add iterative and recursive solutions

--- 9.py
+++ 9.py
@@ -37,6 +37,35 @@ def check_bst(node, parent_value = None):
             res_r = False
     return res_l and res_r
 
+def check_bst_iterative(root):
+    nodes_and_bound_stack = [(root, -float('inf'), float('inf'))]
+    while nodes_and_bound_stack:
+        node, lower_bound, upper_bound = nodes_and_bound_stack.pop()
+        if node.value <= lower_bound or node.value >= upper_bound:
+            return False
+        else:
+            if node.left:
+                nodes_and_bound_stack.append(
+                    (node.left, lower_bound, node.value))
+            if node.right:
+                nodes_and_bound_stack.append(
+                    (node.right, node.value, upper_bound))
+    return True
+
+def check_bst_recursive(node,
+                        lower_bound=-float('inf'),
+                        upper_bound=float('inf')):
+    if not node:
+        return True
+    if node.value <= lower_bound or node.value >= upper_bound:
+        return False
+    return  (
+        check_bst_recursive(node.left, lower_bound, node.value) and
+        check_bst_recursive(node.right, node.value, upper_bound)
+        )
+
+
+
 tree = BinaryTreeNode(50)
 l = tree.insert_left(30)
 l.insert_left(20)
@@ -45,4 +74,6 @@ r = tree.insert_right(80)
 r.insert_left(70)
 r.insert_right(90)
 
-print "Is BST: %s" % check_bst(tree)
+# print "Is BST: %s" % check_bst(tree)
+# print "Is BST: %s" % check_bst_iterative(tree)
+print "Is BST: %s" % check_bst_recursive(tree)
