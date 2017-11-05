d5ab4f276b8e32a9a811d47679a7f9c073685769
Siddhartha Chandra <schandra@collectivei.com>
2017-11-05 16:36:01 -0500

    feat: add function for computing fibonacci in o(1) using Binet's formula

--- 15.py
+++ 15.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 # Compute nth fibonnaci number
 
 # fib(0) # => 0
@@ -5,7 +7,7 @@
 # fib(2) # => 1
 # fib(3) # => 2
 # fib(4) # => 3
-
+# todo: find answer in o(log(n))
 def fib(n):
     ls = [0, 1]
     if n in ls:
@@ -16,7 +18,21 @@ def fib(n):
         ls[1] = res
     return res
 
+# complexity: O(1) using Binet's formula
+def fib_o_1(n):
+    t1 = (float(1 + sqrt(5))/2) ** n
+    t2 = (float(1 - sqrt(5))/2) ** n
+    res = (1/sqrt(5)) * (t1-t2)
+    return res
+
+
 # sample run
 print fib(1)
 print fib(10)
 print fib(100)
+
+# sample run
+print ("Running in Constant time now:")
+print fib_o_1(1)
+print fib_o_1(10)
+print fib_o_1(100)
