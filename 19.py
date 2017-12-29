7d281e67d68c33f8f359a234e3134971d1bc425d
Siddhartha Chandra <schandra@collectivei.com>
2017-12-29 12:53:33 -0500

    P19: add thoughts on complexity

--- 19.py
+++ 19.py
@@ -50,3 +50,16 @@ queue.dequeue()
 queue.dequeue()
 queue.dequeue()
 queue.dequeue()
+
+
+# Complexity
+
+# Each enqueue is clearly O(1) time, and so is each dequeue when out_stack has items. Dequeue on an empty out_stack is order of the number of items in in_stack at that moment, which can vary significantly.
+
+# Notice that the more expensive a dequeue on an empty out_stack is (that is, the more items we have to move from in_stack to out_stack), the more O(1)-time dequeues off of a non-empty out_stack it wins us in the future. Once items are moved from in_stack to out_stackthey just sit there, ready to be dequeued in O(1) time. An item never moves "backwards" in our data structure.
+
+# We might guess that this "averages out" so that in a set of m enqueues and dequeues the total cost of all dequeues is actually justO(m). To check this rigorously, we can use the accounting method, ↴ counting the time cost per item instead of per enqueue or dequeue.
+
+# So let's look at the worst case for a single item, which is the case where it is enqueued and then later dequeued. In this case, the item enters in_stack (costing 1 push), then later moves to out_stack (costing 1 pop and 1 push), then later comes off out_stack to get returned (costing 1 pop).
+
+# Each of these 4 pushes and pops is O(1) time. So our total cost per item is O(1). Our m enqueue and dequeue operations put mm or fewer items into the system, giving a total runtime of O(m).
